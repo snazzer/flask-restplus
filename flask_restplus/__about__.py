0f097a994dffe539886500c4d6024e98870f3987
Nasron Cheong <nasron@integrate.ai>
2019-12-19 14:43:51 -0500

    Dialing down tox so we can run inv all in circleci. Need to use inv all for build to ensure assets are included in package

--- flask_restplus/__about__.py
+++ flask_restplus/__about__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.13.1.dev+iai.1'
+__version__ = '0.13.1.dev+iai.2'
 __description__ = 'Fully featured framework for fast, easy and documented API development with Flask'
